f8631eb5b0c191ec4a802dcd3ec8a5ef74e3dd80
shaunakg <gadkari.shaunak@gmail.com>
2020-09-12 14:56:28 +1000

    selfhosting fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,9 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+
+import 'typeface-inter';
+import './layout.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
